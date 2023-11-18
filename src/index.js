dc0c35de4d35f09235a0837fb7f2345a67a67a43
rajnikant-yadav <rajnikanty61@gmail.com>
2023-11-18 21:34:34 +0530

    Ecommerce Webpage

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import axios from 'axios';
+
+axios.defaults.baseURL = `http://localhost:5001/api`;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
